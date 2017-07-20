9aa44af6fd1b21e497956cb6fc84795a22ddc33a
Dennis Kanzira <Dennis Kanzira@LAPTOP-E0G5NNPS>
2017-07-20 13:12:40 -0400

    created the AntiHero Class. Implemented the AntiHero class In the driver Class

---
+++ Comp123-lesson3b/AntiHero.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ * Name: Dennis Kanzira
+ * Date: July 20, 2017
+ * Description: This is the AntiHero Class.
+ * It extends the SUperHuman base class and implements both IHasKarma and IHasMalice interfaces
+ * Version: 0.1 - created the AntiHero Class.
+ */
+namespace Comp123_lesson3b
+{
+    public class AntiHero : SuperHuman, IHasKarma, IHasMalice
+    {
+        //PRIVATE INSTANCE VARIABLES(FIELDS)
+        private int _karma;
+        private int _malice;
+        //PUBLIC PROPERITIES
+        public int Karma
+        {
+            get
+            {
+                return this._karma;
+            }
+
+            set
+            {
+                this._karma = value;
+            }
+        }
+
+        public int Malice
+        {
+            get
+            {
+                return this._malice;
+            }
+
+            set
+            {
+                this._malice = value;
+            }
+        }
+
+
+        //CONSTRUCTORS -----------------------------------------------------
+        /// <summary>
+        /// This is the maun cnstructor for the AntiHero Class.
+        /// It takes three arguements -name(string) - Karma(int) -
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="karma"></param>
+        /// <param name="malice"></param>
+        public AntiHero(string name, int karma,int malice)
+            : base(name)
+        {
+            this._karma = karma;
+            this._malice = malice;
+        }
+        //PRIVATE METHODS
+
+        //PUBLIC METHODS
+
+    }
+}
--- Comp123-lesson3b/Program.cs
+++ Comp123-lesson3b/Program.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
  * Name:Dennis Kanzira
  * Date:July 11 2017
  * Description: Demo for lesson 9
- * version: 0.4 - Implemented the SuperHero Class
+ * Version: 0.9 - Implemented the AntiHero class
 */
 
 namespace Comp123_lesson3b
@@ -17,22 +17,28 @@ namespace Comp123_lesson3b
     {
         static void Main(string[] args)
         {
-            SuperHero superHero = new SuperHero("SuperDude", 20);
+            SuperHero superHero = new SuperHero("SuperHero Dude", 20);
             superHero.AddPower("Spider Climbing", 50);
-            superHero.AddPower("Web Casting", 40);
-            superHero.AddPower("Phasing", 50);
-            superHero.AddPower("Invisibility", 30);
+            superHero.AddPower("Webcasting", 40);
             Console.WriteLine(superHero.ToString());
+            Console.WriteLine();
             Console.WriteLine("Karma: " + superHero.Karma);
             Console.WriteLine();
 
-            SuperVillain superVillain = new SuperVillain("Buff Dude", 34);
-            superVillain.AddPower("Fireballs", 10);
-            superVillain.AddPower("Stinkbreath", 45);
-            superVillain.AddPower("Blinding smoke", 20);
+            SuperVillain superVillain = new SuperVillain("SuperVillain Dude", 20);
+            superVillain.AddPower("Shadow Casting", 50);
+            superVillain.AddPower("Mind Control", 40);
+            superVillain.AddPower("Combat Senses", 30);
             Console.WriteLine(superVillain.ToString());
-            Console.WriteLine("Malice: " + superVillain.Malice);
             Console.WriteLine();
+            Console.WriteLine("Malice: " + superVillain.Malice);
+
+            AntiHero batman = new AntiHero("Batman", 20, 20);
+            batman.AddPower("Super Intelligence", 50);
+            Console.WriteLine(batman.ToString());
+            Console.WriteLine();
+            Console.WriteLine("Karma: " + batman.Karma);
+            Console.WriteLine("Malice: " + batman.Malice);
 
 
             if (superHero is SuperHero)
